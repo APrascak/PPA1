341b5ea501d508c8cd6583a341dda2fcba61684c
Alex Prascak <aprascak@ufl.edu>
2019-09-08 20:19:54 -0400

    Added command line interface. Finalized and added simple unit test for BMI calculation.

--- ppa1.py
+++ ppa1.py
@@ -2,9 +2,24 @@
 # Alexander Prascak
 
 def making_bmi(weight, height):
-    return (weight > height)
+    return round( ((weight*.45) / (height * .025) ** 2), 1)
 
-if __name__ == "__main__":
-    print("initial test")
-    print(making_bmi(3,5))
-    unittest.main()
+
+if (__name__ == "__main__"):
+    option = 5
+    while option != 0:
+
+        print()
+        print("PRASCAK PPA PROGRAM MENU")
+        print("Enter # for function selection.")
+        print("0. Exit.")
+        print("1. Calculate BMI")
+        option = input("Function #: ")
+        
+        if option == "0":
+            quit()
+        elif option == "1":
+            weight = input("Weight (in pounds): ")
+            height = input("Height (in inches): ")
+            bmi = "BMI: " + str(making_bmi(int(weight), int(height)))
+            print(bmi)
--- test_ppa1.py
+++ test_ppa1.py
@@ -5,7 +5,7 @@ import unittest
 from ppa1 import making_bmi
 
 class TestBMI(unittest.TestCase):
+    
     def test_bmi(self):
-        # Test to make sure equals true
-        self.assertTrue(making_bmi(5,4))
-        self.assertFalse(making_bmi(3,4))
+        # Makes sure example calculations are correct.
+        self.assertEqual(making_bmi(125, 63), 22.7)
