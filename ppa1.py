6f8a6f62e868a711888ca990b6a86da1375bda0b
Alex Prascak <aprascak@ufl.edu>
2019-09-08 21:52:46 -0400

    Finished command line interface. Added all function templates.

--- ppa1.py
+++ ppa1.py
@@ -4,6 +4,18 @@
 def making_bmi(weight, height):
     return round( ((weight*.45) / (height * .025) ** 2), 1)
 
+def retirement(test):
+     return 'Retirement Function Call'
+
+def shortest_distance(test):
+    return 'Shortest Distance Function Call'
+
+def email_verifier(test):
+    return 'Email Verifier Function Call'
+
+def split_the_tip(test):
+    return 'Split the Tip Function Call'
+
 
 if (__name__ == "__main__"):
     option = 5
@@ -12,8 +24,12 @@ if (__name__ == "__main__"):
         print()
         print("PRASCAK PPA PROGRAM MENU")
         print("Enter # for function selection.")
-        print("0. Exit.")
+        print("0. Exit Program")
         print("1. Calculate BMI")
+        print("2. Calculate Retirement Age")
+        print("3. Calculate Shortest Distance")
+        print("4. Email Verifier")
+        print("5. Split the Tip")
         option = input("Function #: ")
         
         if option == "0":
@@ -23,3 +39,11 @@ if (__name__ == "__main__"):
             height = input("Height (in inches): ")
             bmi = "BMI: " + str(making_bmi(int(weight), int(height)))
             print(bmi)
+        elif option == "2":
+            print(retirement(3))
+        elif option == "3":
+            print(shortest_distance(3))
+        elif option == "4":
+            print(email_verifier(3))
+        elif option == "5":
+            print(split_the_tip(3))
